73d8312fbd08efb770b588b5d09125a95f07f92d
syf <1712207189@qq.com>
2022-06-18 17:39:04 +0800

    try to fix some bugs on web

--- makemenu.py
+++ makemenu.py
@@ -11,6 +11,7 @@ out = "README.md"
 
 
 def filelines(file):
+    # return the file's titles
     res = []
     with open(file, "r", encoding="UTF-8") as f:
         for line in f.readlines():
@@ -20,6 +21,28 @@ def filelines(file):
                 if len(line) > 1 and line[0].count("#") == len(line[0]):
                     res.append([len(line[0]), " ".join(line[1:])])
     return res
+
+
+def removespace(file):
+    # remove spaces in file's titles
+    s = ''
+    with open(file, "r", encoding="UTF-8") as f:
+        for i in f.readlines():
+            flag = False
+
+            line = i.strip()
+            if line != '' and not line.isspace():
+                line = line.split()
+                if len(line) > 1 and line[0].count("#") == len(line[0]):
+                    flag = True
+
+            if flag:
+                s += line[0] + " " + "".join(line[1:]) + "\n"
+            else:
+                s += i
+
+    with open(file, "w", encoding="UTF-8") as f:
+        f.write(s)
 
 
 def makemenu(file):
@@ -37,4 +60,5 @@ if __name__ == "__main__":
     with open(out, "w", encoding="UTF-8") as f:
         f.write("""# Templates\n\n""")
         for i in files:
+            removespace(i)
             f.write(makemenu(i))
