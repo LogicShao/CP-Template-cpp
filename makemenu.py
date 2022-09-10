866039f09e61b7a0e15dc5548169bec4374d1977
syf <1712207189@qq.com>
2022-09-10 13:51:30 +0800

    增加了宽搜算法

--- makemenu.py
+++ makemenu.py
@@ -15,8 +15,8 @@ READMEinfo = """# Templates\n
 而且个别模板质量并不高，也希望各位多多包涵  
 如果你有更好的模板或者想法，我很乐意合并你的请求  
 
-另外，本仓库中大部分模板来自 `AcWing算法课` 和 `算法进阶指南`  
-少部分来自网络或个人代码，感谢Ta们对我编程学习的帮助  
+另外，本仓库中大部分模板取自 [AcWing算法全家桶](https://www.acwing.com/activity/content/2142/) 和 [算法进阶指南](https://www.acwing.com/activity/content/6/)  
+知识结构也基本类似，而少部分来自网络或个人代码，感谢Ta们对我编程学习的帮助  
 
 以下是目录:
 """
